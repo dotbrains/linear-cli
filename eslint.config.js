ace10215750372aae291c29d3d422b36baf1e2c0
Nicholas Adamou <nicholasadamouemail@gmail.com>
2026-03-19 03:01:12 -0400

    Add ESLint flat config and @eslint/js dependency

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        process: "readonly",
+        console: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
